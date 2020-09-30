842fcb8ba57bcaf5d1e1f4cc735e41c6f11cbb3b
nik-822 <72146027+nik-822@users.noreply.github.com>
2020-10-01 02:18:05 +0530

    Update remove_empty_row.py

--- remove_empty_row.py
+++ remove_empty_row.py
@@ -1,6 +1,6 @@
 #This program reads in a .csv file called out.csv and removes all empty rows then creates a new file without the rows called FitBit.csv
 
-import csv
+import csv    #install from pip inbuilt function 
 input1 = open('out.csv', 'r')
 output = open('FitBit.csv', 'w', newline='')
 writer = csv.writer(output)
